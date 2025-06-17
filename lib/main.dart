bacf5456c6042a41a8620321613233c4a18f350f
Gustavocorrea0 <gustavocorreaofc@gmail.com>
2025-06-17 19:50:16 -0300

    create project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(Container(
+    child: Text(
+      "Audi",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: const Color.fromARGB(255, 26, 1, 252),
+                       fontSize: 50.0),
+    ),
+  ));
+}
