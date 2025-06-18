6705df8b923afbff0c2c665034b93452588fbd5e
Gustavocorrea0 <gustavocorreaofc@gmail.com>
2025-06-17 21:29:01 -0300

    create StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
+//import 'dart:ui';
 import 'package:flutter/material.dart';
 
+// Estados - Local e Global
+// StatelessWidget
 main(){
-  runApp(Container(
-    child: Text(
-      "Audi",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(color: const Color.fromARGB(255, 26, 1, 252),
-                       fontSize: 50.0),
-    ),
-  ));
+  runApp(AppWidget(
+    title:"Audi Quattro"
+    ));
+}
+// StatelessWidget
+class AppWidget extends StatelessWidget {
+
+  final String title; // Variavel
+
+  const AppWidget({super.key, required this.title}); // construtor
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: const Color.fromARGB(255, 26, 1, 252),
+                          fontSize: 50.0),
+          ),
+        ),
+    );
+  }
+
 }
