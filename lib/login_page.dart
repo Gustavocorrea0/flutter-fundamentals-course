46e3cfc542d7e5ed745dedd70dfc98a9c0864ea8
Gustavocorrea0 <gustavocorreaofc@gmail.com>
2025-06-23 21:30:57 -0300

    use card

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -29,50 +29,75 @@ class _LoginPageState extends State<LoginPage> {
                   //child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/600px-BMW_logo_%28gray%29.svg.png"),
                   child: Image.asset('assets/images/logo-bmw.png')
                 ),
-
                 Container(height: 40),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 12, 
+                                                   right: 12,
+                                                   top: 20,
+                                                   bottom: 12),
+                    child: Column(
+                      children: [
+                          TextField(
+                            onChanged: (text) {
+                              email = text;
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              labelText: "Email",
+                              border: OutlineInputBorder()
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextField(
+                            onChanged: (text) {
+                              password = text;
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: "Password",
+                              border: OutlineInputBorder()
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          ElevatedButton(
+                              onPressed: () {
+                                if (email == "gustavo@alfredo.com" && password == "admin") {
+                                  //print("Correto");
+                                  //Navigator.of(context).push(
+                                  //  MaterialPageRoute(builder: (context) => HomePage()) // navegacao manual - com sobreposicao (retorno)
+                                  //);
+                                  //Navigator.of(context).pushReplacement(
+                                  //  MaterialPageRoute(builder: (context) => HomePage()) // navegacao manual - sem sobreposicao (retorno)
+                                  //);
 
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    border: OutlineInputBorder()
+                                  //Navigator.of(context).pushNamed("/home"); // navegacao nomeadas - com sobreposicao (retorno) 
+                                  Navigator.of(context).pushReplacementNamed("/home"); // navegacao nomeadas - sem sobreposicao (retorno)
+                                } else {
+                                  print("Login Invalido");
+                                }
+                              },
+                              // formatacao do botao
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue, // Cor de fundo
+                                foregroundColor: Colors.white, // Cor do texto
+                                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12), // opcional
+                                shape: RoundedRectangleBorder( // opcional: borda arredondada
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                              ),
+
+                              child: Container(
+                                width: double.infinity,
+                                child: Text("Entrar", textAlign: TextAlign.center)
+                              ),
+                              
+                          )
+                      ],
+                    ),
                   ),
                 ),
                 SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == "gustavo@alfredo.com" && password == "admin") {
-                      //print("Correto");
-                      //Navigator.of(context).push(
-                      //  MaterialPageRoute(builder: (context) => HomePage()) // navegacao manual - com sobreposicao (retorno)
-                      //);
-                      //Navigator.of(context).pushReplacement(
-                      //  MaterialPageRoute(builder: (context) => HomePage()) // navegacao manual - sem sobreposicao (retorno)
-                      //);
-
-                      //Navigator.of(context).pushNamed("/home"); // navegacao nomeadas - com sobreposicao (retorno) 
-                      Navigator.of(context).pushReplacementNamed("/home"); // navegacao nomeadas - sem sobreposicao (retorno)
-                    } else {
-                      print("Login Invalido");
-                    }
-                  },
-                  child: Text("Entrar")
-                )
               ],
             ),
           ),
